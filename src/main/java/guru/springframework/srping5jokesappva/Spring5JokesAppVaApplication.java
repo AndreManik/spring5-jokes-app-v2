dc70a285490b685b953765b0a23e4b9570232746
manik4 <andrei.manik@orionic.com>
2023-03-06 13:39:38 +0300

    First init commit

---
+++ src/main/java/guru/springframework/srping5jokesappva/Spring5JokesAppVaApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.srping5jokesappva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppVaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokesAppVaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/srping5jokesappva/Srping5JokesAppVaApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.srping5jokesappva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Srping5JokesAppVaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
